d09f32b9a22a9bb236d4e40116746a30bc309d57
Andrew Reedy <andrewrreedy@gmail.com>
2015-03-03 03:12:12 -0800

    updated to 1.0

--- package.js
+++ package.js
@@ -1,21 +1,31 @@
 Package.describe({
-  summary: "Enable the application cache in the browser (with extras)",
+  name: "hybrid:appcache-extra"
+  summary: "Meteor's appcache + ability to prompt + manually add files",
   version: "0.1.0",
-  name: "andrewreedy/appcache-extra",
-  git: "https://github.com/andrewreedy/meteor-appcache-extra"
+  git: "https://github.com/meteorhybrid/appcache-extra"
 });
 
 Package.onUse(function (api) {
-  api.use('webapp', 'server');
-  api.use('reload', 'client');
-  api.use('templating', 'client');
-  api.use('routepolicy', 'server');
-  api.use('underscore', 'server');
+  api.versionsFrom('METEOR@1.0');
+
+  api.use([
+    'reload',
+    'templating'
+  ], 'client');
+
+  api.use([
+    'routepolicy',
+    'webapp',
+    'underscore'
+  ] , 'server');
+
   api.use('autoupdate', 'server', {weak: true});
+
   api.addFiles([
     'appcache-prompt.html',
     'appcache-prompt.css',
     'appcache-client.js'
   ], 'client');
+  
   api.addFiles('appcache-server.js', 'server');
 });
